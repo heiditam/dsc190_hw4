771e769789de90882b48a55eb082c16e816b9777
Heidi Tam <125783723+heiditam@users.noreply.github.com>
2024-11-24 12:08:33 -0800

    5 sun nov 24 12:08 pm

--- hw4.R
+++ hw4.R
@@ -1,4 +1,23 @@
 # QUESTION 1
+model <- lm(gain ~ density, data = data) # fit a regression model
+summary(model)
+
+# graph: scatterplot
+plot(data$density, data$gain, main = 'Gamma Ray Intensity as a Function of the Density of Polyethylene Blocks', xlab = 'Density of Polyethylene Blocks', ylab = 'Gamma Ray Intensity')
+abline(model, col = 'red') # plot line of best fit
+
+res <- resid(model)
+plot(fitted(model), res, xlab='Predicted Gamma Ray Intensity', ylab = 'Residuals', main = 'Residual vs. Fitted Plot of Model')
+abline(0,0)
+
+qqnorm(res)
+qqline(res)
+
+plot(density(res))
+
+set.seed(123)
+shapiro_result <- shapiro.test(data$gain)
+print(shapiro_result)
 
 # QUESTION 2
 
